1d5b1553aadad3b17f4c16c250af3cffa70a74e8
Jhea Prudencio <2232799@slu.edu.ph>
2025-08-23 21:57:23 +0800

    fixed layout

--- src/App.jsx
+++ src/App.jsx
@@ -13,12 +13,12 @@ const App = () => {
         <section className="relative flex flex-col items-center justify-center w-full h-screen text-center text-gray-900 overflow-hidden">
             <img src="src/assets/Landing-page/bgleft.png" alt="background" className="absolute left-0 top-0 h-full object-contain -z-10"/>
             <img src="src/assets/Landing-page/bgright.png" alt="background" className="absolute right-0 top-0 h-full object-contain -z-10"/>
-            <img src="src/assets/Landing-page/igorot.png" alt="background" className="absolute right-0 bottom-0 w-1/2 object-contain -z-15"/>
+            <img src="src/assets/Landing-page/igorot.png" alt="background" className="absolute right-0 bottom-0 w-1/2 object-contain -z-10"/>
 
-            <div className="relative bottom-17 right-35">
-                <h1 className="text-7xl pb-5 font-bold">One Cordillera</h1>
-                <h2 className="text-4xl md:text-5xl pb-3 mt-2">A Glimpse of the <br /> Mountainous Region</h2>
-                <p className="text-3xl mt-2">DENR - CAR <br/> 2025 </p>
+            <div className="relative bottom-17 right-45">
+                <h1 className="text-8xl pb-5 font-bold">One Cordillera</h1>
+                <h2 className="text-5xl md:text-6xl pb-3 mt-2">A Glimpse of the <br /> Mountainous Region</h2>
+                <p className="text-4xl mt-2">DENR - CAR <br/> 2025 </p>
 
                 <div className="flex justify-center gap-4 mt-6">
                     <button className="text-xl px-6 py-2 rounded-lg bg-green-600 text-white cursor-pointer hover:scale-110 transition">
@@ -45,30 +45,30 @@ const App = () => {
             <div className="absolute inset-0 bg-black/40 -z-0"></div>
 
             <div className="relative z-10 flex flex-col items-center space-y-20">
-                <h1 className="text-5xl font-bold text-white">ABOUT</h1>
+                <h1 className="text-7xl font-bold text-white pb-25">ABOUT</h1>
                 
-                <div className="grid grid-cols-1 md:grid-cols-4 gap-5 text-center">
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-10 text-center pb-25">
                 <button className="flex flex-col items-center space-y-3 text-white hover:scale-110 transition">
-                    <img src="src/assets/About-images/CAR.svg" alt="CAR" className="w-23 h-20" />
-                    <span className="text-lg font-medium">CAR</span>
+                    <img src="src/assets/About-images/CAR.svg" alt="CAR" className="w-35 h-30" />
+                    <span className="text-xl font-medium">CAR</span>
                 </button>
 
                 {/* Biophysical Profile */}
                 <button className="flex flex-col items-center space-y-3 text-white hover:scale-110 transition">
-                    <img src="src/assets/About-images/Biophysical.svg" alt="Biophysical Profile" className="w-20 h-20" />
-                    <span className="text-lg font-medium">Biophysical Profile</span>
+                    <img src="src/assets/About-images/Biophysical.svg" alt="Biophysical Profile" className="w-30 h-30" />
+                    <span className="text-xl font-medium">Biophysical Profile</span>
                 </button>
 
                 {/* ENR Resources */}
                 <button className="flex flex-col items-center space-y-3 text-white hover:scale-110 transition">
-                    <img src="src/assets/About-images/Earth.svg" alt="ENR Resources" className="w-23 h-20" />
-                    <span className="text-lg font-medium">ENR Resources</span>
+                    <img src="src/assets/About-images/Earth.svg" alt="ENR Resources" className="w-33 h-30" />
+                    <span className="text-xl font-medium">ENR Resources</span>
                 </button>
 
                 {/* Socio-Cultural and Economic Profile */}
                 <button className="flex flex-col items-center space-y-3 text-white hover:scale-110 transition">
-                    <img src="src/assets/About-images/SCE.svg" alt="Socio-Cultural and Economic Profile" className="w-29 h-20" />
-                    <span className="text-lg font-medium text-center">
+                    <img src="src/assets/About-images/SCE.svg" alt="Socio-Cultural and Economic Profile" className="w-39 h-30" />
+                    <span className="text-xl font-medium text-center">
                     Socio-Cultural and Economic Profile
                     </span>
                 </button>
@@ -77,28 +77,28 @@ const App = () => {
         </section>
         
         {/* CAR */}
-        <section className="flex flex-col items-center justify-center  min-h-screen w-screen overflow-hidden m-0 p-0 pl-25 pr-25 py-5 bg-gradient-to-b from-[#3b82f640] to-white">
-            <h1 className="text-5xl pb-15 font-bold">CORDILLERA ADMINISTRATIVE REGION</h1>
-            <div className="flex flex-wrap gap-20 pb-15 justify-center">
-                <img src="src/assets/Logos/Province-of-Benguet-Logo.png" alt="Province of Benguet Logo" className="w-32 h-32" />
-                <img src="src/assets/Logos/Province-of-Kalinga-Logo.png" alt="Province of Kalinga Logo" className="w-32 h-32 rounded-lg" />
-                <img src="src/assets/Logos/Mountain-Province-Logo.png" alt="Mountain Province Logo" className="w-32 h-32 rounded-lg" />
-                <img src="src/assets/Logos/Province-of-Abra-Logo.png" alt="Province of Abra Logo" className="w-32 h-32 rounded-lg" />
-                <img src="src/assets/Logos/Province-of-Apayao-Logo.png" alt="Province of Apayao Logo" className="w-32 h-32 rounded-lg" />
-                <img src="src/assets/Logos/Province-of-Ifugao-Logo.png" alt="Province of Ifugao Logo" className="w-32 h-32 rounded-lg" />
+        <section className="flex flex-col items-center justify-center  min-h-screen w-screen overflow-hidden m-0 p-0 pl-25 pr-25 bg-gradient-to-b from-[#3b82f640] to-white">
+            <h1 className="text-7xl pb-20 font-bold">CORDILLERA ADMINISTRATIVE REGION</h1>
+            <div className="flex flex-wrap gap-20 pb-20 justify-center">
+                <img src="src/assets/Logos/Province-of-Benguet-Logo.png" alt="Province of Benguet Logo" className="w-45 h-45" />
+                <img src="src/assets/Logos/Province-of-Kalinga-Logo.png" alt="Province of Kalinga Logo" className="w-45 h-45 rounded-lg" />
+                <img src="src/assets/Logos/Mountain-Province-Logo.png" alt="Mountain Province Logo" className="w-45 h-45 rounded-lg" />
+                <img src="src/assets/Logos/Province-of-Abra-Logo.png" alt="Province of Abra Logo" className="w-45 h-45 rounded-lg" />
+                <img src="src/assets/Logos/Province-of-Apayao-Logo.png" alt="Province of Apayao Logo" className="w-45 h-45 rounded-lg" />
+                <img src="src/assets/Logos/Province-of-Ifugao-Logo.png" alt="Province of Ifugao Logo" className="w-45 h-45 rounded-lg" />
 
             </div>
-            <p className="text-xl text-justify">
+            <p className="text-3xl text-justify pl-25 pr-25 pb-5">
                 The Cordillera Administrative Region (CAR) was established on July 15, 1987
                 through Executive Order No. 220, signed by then President Corazon Aquino.
             </p>
-            <p className="text-xl text-justify">
+            <p className="text-3xl text-justify pl-25 pr-25 pb-5">
                 The region consists of the provinces of Abra, Apayao, Benguet, Ifugao, Kalinga, and
                 Mountain Province. Its regional center is the Chartered City of Baguio, officially
                 known as the Summer Capital of the Philippines. 
             </p>
-            <p className="text-xl text-justify">
-                Cordillera is one of the prime tourist destinations in the Philippines,It has many
+            <p className="text-3xl text-justify pl-25 pr-25">
+                Cordillera is one of the prime tourist destinations in the Philippines. It has many
                 spectacular scenic views and enchantingly cool places. The world-famous Banaue Rice
                 Terraces in the province of Ifugao is considered as the "Eight Wonder of the World". 
                 This structure of about 2000 to 6000 years old is a United Nations Organization for 
@@ -109,15 +109,20 @@ const App = () => {
         </section>
 
         {/* BIOPHYSICAL PROFILE */}
-        <div  className="relative flex flex-col items-center justify-center space-y-6 bg-repeat-x bg-contain"
-        style={{ backgroundImage: "url('src/assets/Biophysical-images/bg.png')" }} >
-            <h1 className="text-5xl pt-10 pb-8 font-bold text-white">BIOPHYSICAL PROFILE</h1>
+        <div className="flex flex-col items-center justify-center">
+            {/* Title with thinner background */}
+            <div
+                className="w-full flex items-center justify-center py-10 bg-cover bg-center"
+                style={{ backgroundImage: "url('src/assets/Biophysical-images/bg.png')" }}
+            >
+                <h1 className="text-6xl font-bold text-white">BIOPHYSICAL PROFILE</h1>
+            </div>
  
             <div className="flex flex-col items-center mt-6 space-y-8">
                 
                 <div className="flex flex-col md:flex-row items-center gap-6 w-full pr-25 pl-25">
-                    <p className="text-lg text-justify flex-1 pr-15">
-                    <span className="text-2xl font-semibold">
+                    <p className="text-2xl text-justify flex-1 pr-15">
+                    <span className="text-4xl font-semibold">
                     Geographical Location, Land Area and Political Subdivision.
                     </span>
                     <br></br>Cordillera Administrative Region is the only landlocked region in the
@@ -130,19 +135,19 @@ const App = () => {
                     <img
                         src="src/assets/Biophysical-images/Anguib Beach Cagayan.png"
                         alt="Anguib Beach Cagayan"
-                        className="w-70 h-50 object-cover rounded-lg shadow"
+                        className="w-95 h-50 object-cover rounded-lg shadow"
                     />
                     <p className="text-sm mt-2">Anguib Beach Cagayan</p>
                     </div>
                 </div>
 
                 {/* Row 2: 4 Images */}
-                <div className="grid grid-cols-2 md:grid-cols-4 gap-13">
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-20 pb-5">
                     <div className="flex flex-col items-center">
                     <img
                         src="src/assets/Biophysical-images/Enchanted Cave Pangasinan.png"
                         alt="Enchanted Cave Pangasinan"
-                        className="w-70 h-50 object-cover rounded-lg shadow"
+                        className="w-95 h-50 object-cover rounded-lg shadow"
                     />
                     <p className="text-sm mt-2">Enchanted Cave Pangasinan</p>
                     </div>
@@ -150,7 +155,7 @@ const App = () => {
                     <img
                         src="src/assets/Biophysical-images/Bangui Windmills Ilocos Norte.png"
                         alt="Bangui Windmills Ilocos Norte"
-                        className="w-70 h-50 object-cover rounded-lg shadow"
+                        className="w-95 h-50 object-cover rounded-lg shadow"
                     />
                     <p className="text-sm mt-2">Bangui Windmills Ilocos Norte</p>
                     </div>
@@ -158,7 +163,7 @@ const App = () => {
                     <img
                         src="src/assets/Biophysical-images/Mt. Malusong Summit Nueva Viscaya.png"
                         alt="Mt. Malusong Summit Nueva Viscaya"
-                        className="w-70 h-50 object-cover rounded-lg shadow"
+                        className="w-95 h-50 object-cover rounded-lg shadow"
                     />
                     <p className="text-sm mt-2">Mt. Malusong Summit Nueva Viscaya</p>
                     </div>
@@ -166,37 +171,43 @@ const App = () => {
                     <img
                         src="src/assets/Biophysical-images/Calle Crisologo Ilocos Sur.png"
                         alt="Calle Crisologo Ilocos Sur"
-                        className="w-70 h-50 object-cover rounded-lg shadow"
+                        className="w-95 h-50 object-cover rounded-lg shadow"
                     />
                     <p className="text-sm mt-2">Calle Crisologo Ilocos Sur</p>
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col items-center pl-25 pr-25 pb-10">
-                <p className="flex text-xl text-justify pb-5">
+            <div className="flex flex-col items-center pl-25 pr-25">
+                <p className="flex text-2xl text-justify">
                     It is politically subdivided into 77 municipalities and 1,176 barangays that makes up to 6.6 percent of the total land area of
                     the Philippines. The total area of the region of about 1,865,454 hectares based on the approved cadastral boundary map. The 
                     Alienable and Disposable lands is 15.05% of the total land area while 84.95% is forestland. Mountain Province has the least 
                     area coverage with a total of 231,486 hectares. The chartered city of Baguio is less than 1% of the total area of the region.
                 </p>
                 <Link to="/biophysical">
-                    <button className="px-10 py-2 mt-4 rounded-lg bg-blue-600 text-white cursor-pointer hover:scale-110 transition">
+                    <button className="text-lg px-15 py-2 mt-6 rounded-lg bg-blue-600 text-white cursor-pointer hover:scale-110 transition">
                         See More
                     </button>
                 </Link>
             </div>
         </div>
 
-        {/* DENR RESOURCES */}
+        {/* ENR RESOURCES */}
         <section>
-            <div className="flex flex-row items-center justify-center space-x-6 pl-20 pr-15"> 
-                <h1 className="text-5xl font-bold">DENR RESOURCES</h1>
-                    <p className="text-xl text-justify">
+            <div className="flex flex-row items-center justify-center space-x-6 pr-25"> 
+                <div className="w-full flew items-center justify-center py-10 bg-cover bg-center"
+                    style={{backgroundImage: "url('src/assets/Biophysical-images/ENR.png"}}>
+                    <h1 className="text-5xl font-bold text-white text-center">ENR RESOURCES</h1>
+                </div>
+                <div className="flex flex-row items-center justify-center space-x-6 py-10">
+                    <p className="text-2xl text-justify">
                         The Cordillera Region is very rich in natural resources. It is especially famed for its huge gold
                         deposits, pure stands of pine forest, and rich soils and water sources that have enabled its people
                         to sustain agriculture on mountainside rice terraces.
                     </p>
+                </div>
             </div>
+
             {/* Land Resources */}
             <div className="flex flex-col space-y-6 py-12 pl-20 pr-15">
                 <h2 className="text-3xl">Land Resources</h2>
